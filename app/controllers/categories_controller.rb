9788f8ecfef75de6fccc1988822b49945718bad3
Mauricio Cen <mauricio1998.cen@gmail.com>
2022-03-12 12:40:49 -0600

    user test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,4 +10,31 @@ class CategoriesController < ApplicationController
     category = Category.find(params[:id])
     render json: category
   end
+
+  def create
+    category = Category.create!(create_params)
+    render json: category, status: :created
+  end
+
+  def update
+    category = Category.find(params[:id])
+    category.update!(update_params)
+    render json: category
+  end
+
+  def destroy
+    category = Category.find(params[:id])
+    category.destroy!
+    render json: category, status: :no_content
+  end
+
+  private
+
+  def create_params
+    params.require(:category).permit(:name)
+  end
+
+  def update_params
+    params.require(:category).permit(:name)
+  end
 end
