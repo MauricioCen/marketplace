529a136bb44d8a8661acca160079ac7420b4e391
Mauricio Cen <mauricio1998.cen@gmail.com>
2022-03-19 11:18:15 -0600

    devise

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -4,12 +4,16 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string
-#  last_name  :string
-#  email      :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                     :integer          not null, primary key
+#  name                   :string
+#  last_name              :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
 #
 class UserSerializer < ApplicationSerializer
   attributes :id, :name, :last_name, :email
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,17 +4,21 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string
-#  last_name  :string
-#  email      :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                     :integer          not null, primary key
+#  name                   :string
+#  last_name              :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
 #
 FactoryBot.define do
   factory :user do
     name { FFaker::Name.first_name }
     last_name { FFaker::Name.last_name }
-    email { FFaker::Internet.email }
+    email { FFaker::Internet.unique.email }
   end
 end
