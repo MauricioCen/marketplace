2b3dfe20ef6bd8ac29e62480d33d869ccff4c228
Mauricio Cen <mauricio1998.cen@gmail.com>
2022-04-01 14:32:29 -0600

    add query promotions

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -2,7 +2,14 @@
 
 class PromotionsController < ApplicationController
   def index
-    _pagy, products = pagy(Product.where('discount > 0'), items: params[:size])
+    promotions = Product.where('discount > 0')
+    filter = params[:q]
+    query = if filter.blank?
+              promotions
+            else
+              promotions.search(filter)
+            end
+    _pagy, products = pagy(query, items: params[:size])
     render json: products
   end
 end
--- spec/requests/promotions_spec.rb
+++ spec/requests/promotions_spec.rb
@@ -4,17 +4,23 @@ require 'rails_helper'
 
 RSpec.describe 'promotions', type: :request do
   describe 'GET /promotions' do
-    let!(:product_one) { create(:product, discount: 1) }
-    let!(:product_two) { create(:product, discount: 3) }
-    let!(:product_three) { create(:product, discount: 5) }
-    let!(:product_four) { create(:product, discount: 7) }
-    let!(:product_five) { create(:product, discount: 9) }
-    let!(:product_six) { create(:product, discount: 0) }
+    let!(:product_one) { create(:product, discount: 1, name: 'PC Gamer') }
+    let!(:product_two) { create(:product, discount: 3, name: 'Laptop') }
+    let!(:product_three) { create(:product, discount: 5, name: 'PC Gamer2') }
+    let!(:product_four) { create(:product, discount: 7, name: 'Laptop2') }
+    let!(:product_five) { create(:product, discount: 9, name: 'Teclado') }
+    let!(:product_six) { create(:product, discount: 0, name: 'Mouse') }
 
     it 'should return status code 200' do
       get '/promotions', params: { page: 1, size: 10 }
       payload = JSON.parse(response.body)
       expect(payload.size).to eq(5)
+      expect(response).to have_http_status(:ok)
+    end
+    it 'should return 2 promotions ' do
+      get '/promotions', params: { q: 'PC Gamer' }
+      payload = JSON.parse(response.body)
+      expect(payload.size).to eq(2)
       expect(response).to have_http_status(:ok)
     end
   end
