55e5942fddcfc148959b09c0b49349daf64477d5
Mauricio Cen <mauricio1998.cen@gmail.com>
2022-04-03 12:55:25 -0500

    add query index categories/products

--- spec/requests/categories/products_spec.rb
+++ spec/requests/categories/products_spec.rb
@@ -5,12 +5,22 @@ require 'rails_helper'
 RSpec.describe 'categories/products', type: :request do
   describe 'GET /categories/:category_id/products' do
     let!(:category) { create(:category) }
-    let!(:products) { create_list(:product, 6, category_id: category.id) }
+    let!(:product_one) { create(:product, category_id: category.id, name: 'PC Gamer') }
+    let!(:product_two) { create(:product, category_id: category.id, name: 'Foco') }
+    let!(:product_three) { create(:product, category_id: category.id, name: 'Foco2') }
+    let!(:product_four) { create(:product, category_id: category.id, name: 'PC Gamer2') }
+    let!(:product_five) { create(:product, category_id: category.id, name: 'Foco3') }
 
-    it 'should return 6 products' do
-      get "/categories/#{category.id}/products", params: { page: 1, size: 2 }
+    it 'should return products' do
+      get "/categories/#{category.id}/products", params: { page: 1, size: 10 }
       payload = JSON.parse(response.body)['products']
-      expect(payload.size).to eq(2)
+      expect(payload.size).to eq(5)
+      expect(response).to have_http_status(:ok)
+    end
+    it 'should return 3 products' do
+      get '/products', params: { q: 'Foco' }
+      payload = JSON.parse(response.body)['products']
+      expect(payload.size).to eq(3)
       expect(response).to have_http_status(:ok)
     end
   end
